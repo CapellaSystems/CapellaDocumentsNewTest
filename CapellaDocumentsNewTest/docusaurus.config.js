8b99ca147ed922459ec99e352b2069f4a31d6c35
CapellaDocuments <188106524+CapellaDocuments@users.noreply.github.com>
2025-03-12 16:32:32 -0700

    Update NodeModules

--- CapellaDocumentsNewTest/docusaurus.config.js
+++ CapellaDocumentsNewTest/docusaurus.config.js
@@ -15,18 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://CapellaSystems.github.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CapellaDocumentsNewTest/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CapellaSystems', // Usually your GitHub org/user name.
-  projectName: 'CapellaDocumentsNewTest', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
-  
-  onBrokenLinks: 'warn',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -77,25 +76,13 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Capella Documentation',
+        title: 'My Site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
         },
         items: [
-		  {
-            type: 'docSidebar',
-            sidebarId: 'StreamSidebar',
-            position: 'left',
-            label: 'Cambria Stream',
-          },
           {
-            type: 'docSidebar',
-            sidebarId: 'FTCSidebar',
-            position: 'left',
-            label: 'Cambria FTC',
-          },
-          /*{
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
@@ -106,7 +93,7 @@ const config = {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
             position: 'right',
-          },*/
+          },
         ],
       },
       footer: {
